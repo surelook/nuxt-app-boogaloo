5d3967d68d5bee928652b95e8883ade6fada7d4c
Marc Carroll <marc@deveire.com>
2022-09-23 09:40:28 +0100

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,9 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    baseURL: process.env.BASE_URL,
-    publicRuntimeConfig: {
-        gaTrackingId: process.env.GA_TRACKING_ID,
+    app: {
         baseURL: process.env.BASE_URL
+    },
+    publicRuntimeConfig: {
+        gaTrackingId: process.env.GA_TRACKING_ID
     }
 })
