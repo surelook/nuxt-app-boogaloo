714afdcea149e645e9a50114e2cca3c0f3178e76
Marc Carroll <marc@deveire.com>
2022-09-23 09:03:57 +0100

    Go

---
+++ app.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <dl style="border: 1px solid blue">
+      <dt>
+        Test Var
+      </dt>
+      <dd>
+        {{testVar}}
+      </dd>
+
+    </dl>
+    <NuxtWelcome />
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        testVar: process?.env?.TEST_VAR
+      }
+    }
+  }
+</script>
